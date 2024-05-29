3898eeb49b801e5dc6eee8916ba5696dfdaa2d1d
SushantShelake <124454809+sushant2209@users.noreply.github.com>
2024-05-29 11:02:25 +0530

    Delete Parking - Copy/remopark directory

--- Parking - Copy/remopark/smartapp/trial.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime
-
-# Get the current time
-current_time = datetime.now()
-
-# Extract the hour
-current_hour = current_time.hour
-
-print("Current Hour:", current_hour)
