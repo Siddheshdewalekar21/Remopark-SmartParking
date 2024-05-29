1cc97dd599083a5cfdac166117e3ff538b6b6451
SushantShelake <124454809+sushant2209@users.noreply.github.com>
2024-05-29 11:03:38 +0530

    Add files via upload

---
+++ smartapp/trial.py
@@ -0,0 +1,9 @@
+from datetime import datetime
+
+# Get the current time
+current_time = datetime.now()
+
+# Extract the hour
+current_hour = current_time.hour
+
+print("Current Hour:", current_hour)
